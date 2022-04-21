5ae324afa459818138d027ebe6c9988a609be9e2
Elliot064 <104162566+Elliot064@users.noreply.github.com>
2022-04-21 14:22:11 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
 public class Main{
     public static void main(String[] args){
         for(int i = 1; i<=100;i++){
-        if(i%3==0&&i%5==0){
+        if(i%3==0&&i%5==0){    // handles numbers that are multiples of 3 and 5
           System.out.println("FooBaa");
         }
-        else if(i%3==0){
+        else if(i%3==0){  // handles numbers that are multiples of 3
           System.out.println("Foo");
         }
-        else if(i%5==0){
+        else if(i%5==0){ // handles numbers that are multiples of 5
           System.out.println("Baa");
         }
-        else{
+        else{             // all other cases
           System.out.println(i);
         }
       }
